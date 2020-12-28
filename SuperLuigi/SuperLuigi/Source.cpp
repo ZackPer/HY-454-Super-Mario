4e760fc35a003d4f5f70c1c5538dd3f244a8257b
Antos2 <antosdapro@gmail.com>
2020-12-28 04:05:01 +0200

    Tiles mostly done -View window spawns and displays part of map -Refactoring in order for the code to be cleaner including:     some wrapper methods in main     class ViewWindow that contains all the view window needs     change in the position of some methods and/or variables TODO: More refactoring

--- SuperLuigi/SuperLuigi/Source.cpp
+++ SuperLuigi/SuperLuigi/Source.cpp
@@ -3,71 +3,56 @@
 #include <allegro5/allegro_image.h>
 #include <iostream>
 #include "Tiles.h"
-#include <vector>
-#include <fstream>
-#include <string>
-#include <sstream>
 
-#define WIDTH	1024
-#define	HEIGHT	768
+#define WIDTH	640
+#define	HEIGHT	480
 
-std::vector<std::vector<int>> getTileMapIDs(std::string filename) {
-	std::string line;
-	std::vector<std::vector<int>> TileIDs;
-	std::ifstream input(filename);
 
-	int id;
-	if (input.is_open())
-		while (std::getline(input, line)) {
-			std::vector<int> row;
-			std::stringstream input_stringstream(line);
-			while (getline(input_stringstream, line, ',')) {
-				row.push_back(std::stoi(line));
-			}
-			TileIDs.push_back(row);
-		}
-	return TileIDs;
+
+void initViewWindow(
+	int dimHeight, int dimWidth, int dimX, int dimY,
+	int dispHeight, int dispWidth, int dispX, int dispY,
+	ViewWindow& win
+){
+	win = ViewWindow(
+		Rect( dimX, dimY, dimHeight, dimWidth),
+		Rect(dispX, dispY, dispHeight, dispWidth)
+	);
 }
-
-int getColFromID(int ID, int TileSetWidth) {
-	return ID % TileSetWidth;
-}
-
-int getRowFromID(int ID, int TileSetWidth) {
-	return ID / TileSetWidth;
-}
-
 
 int main() {
 	ALLEGRO_DISPLAY *display;
-	ALLEGRO_BITMAP* image = nullptr;
 	ALLEGRO_EVENT_QUEUE *event_queue = nullptr;
-
-	ALLEGRO_BITMAP* tileset = nullptr;
-	ALLEGRO_BITMAP* screenBitmap = nullptr;
-
-	std::vector<std::vector<int>> TileMapIndexes = getTileMapIDs("CSVMaps/mario.csv");
+	TileMapIndexes = getTileMapIDs("CSVMaps/mario1.csv");
+	int mapColumns, mapRows;
+	mapRows = TileMapIndexes.size();
+	mapColumns = TileMapIndexes[0].size();
 
 	if(!al_init())
 		return -1;
-
 	display = al_create_display(WIDTH, HEIGHT);
-
-	if (!display)
-		return 1;
 
 	//allegro addons
 	al_install_keyboard();
 	al_init_image_addon();
 
-	image = al_load_bitmap("Tiles/super_mario_tiles.png");
-	screenBitmap = al_create_bitmap(WIDTH, HEIGHT);
-
-	if (!image)
-		return -1;
+	tileset = al_load_bitmap("Tiles/super_mario_tiles.png");
+	map = al_create_bitmap(mapRows*TILE_HEIGHT, mapColumns*TILE_WIDTH);
 
 	event_queue = al_create_event_queue();
 	al_register_event_source(event_queue, al_get_keyboard_event_source());
+
+	//mapping map indexes to tilesetIndexes
+	TileMap mapTileIndexes;
+	getMapIndexes(mapTileIndexes, TileMapIndexes);
+
+	//initializing view window
+	ViewWindow win1;
+	initViewWindow(
+		160, 160, 0, 0,
+		0, 0, 0, 0,
+		win1
+	);
 
 	while (1) {
 		ALLEGRO_EVENT ev;
@@ -77,18 +62,13 @@ int main() {
 				break;
 			}
 		}
-		for (int i = 0; i < TileMapIndexes.size(); i++) {
-			for (int j = 0; j<TileMapIndexes[i].size(); j++) {
-				PutTile(screenBitmap, i*16, j*16, image, MakeIndex2(getRowFromID(TileMapIndexes[j][i], 19), getColFromID(TileMapIndexes[j][i], 19)));
-			}
-		}
-
+		TileTerrainDisplay(mapTileIndexes, win1.camera, win1.dimensions, win1.displayArea);
 		al_set_target_bitmap(al_get_backbuffer(display));
-		al_draw_scaled_bitmap(screenBitmap, 0, 0, WIDTH, HEIGHT, 0, 0, WIDTH * TILE_SCALE, HEIGHT * TILE_SCALE,0);
+		al_draw_scaled_bitmap(win1.camera, 0, 0, 160, 160, 0, 0, 160, 160 ,0);
 		al_flip_display();
 	}
 
-	al_destroy_bitmap(image);
+	al_destroy_bitmap(tileset);
 	al_destroy_event_queue(event_queue);
 	al_destroy_display(display);
 
--- SuperLuigi/SuperLuigi/Tiles.h
+++ SuperLuigi/SuperLuigi/Tiles.h
@@ -1,20 +1,39 @@
 #include <allegro5/allegro.h>
 #include <stdio.h>
-//slide 12 dialeksh 6
+#include <vector>
+
+#include <fstream>
+#include <string>
+#include <sstream>
 typedef unsigned short Dim;
-struct Rect { int x, y, w, h; };
-struct Point { int x, y; };
+struct Rect { 
+	int x, y, w, h; 
+	Rect(int x, int y, int w, int h) {
+		this->x = x;
+		this->y = y;
+		this->w = w;
+		this->h = h;
+	}
+};
+struct Point { 
+	int x, y; 
+	Point(int x, int y) {
+		this->x = x;
+		this->y = y;
+	}
+};
 enum BitDepth { bits8 = 1, bits16, bits24, bits32 };
+typedef unsigned short Index;
+typedef std::vector<std::vector<Index>> TileMap;
+typedef unsigned char byte;
 
-
-//slide 12 dialeskh 8
 #define TILE_WIDTH 16
 #define TILE_HEIGHT 16
 #define ROW_MASK 0x0F
 #define COL_MASK 0xF0
 #define COL_SHIFT 4
 
-typedef unsigned char byte;
+
 byte MakeIndex(byte row, byte col)
 {
 	return (col << COL_SHIFT) | row;
@@ -28,15 +47,6 @@ byte GetRow(byte index)
 	return index & ROW_MASK;
 }
 
-Dim TileX(byte index)
-{
-	return GetCol(index) * TILE_WIDTH;
-}
-Dim TileY(byte index)
-{
-	return GetRow(index) * TILE_HEIGHT;
-}
-
 #define MUL_TILE_WIDTH(i) ((i)<<4)
 #define MUL_TILE_HEIGHT(i) ((i)<<4)
 #define DIV_TILE_WIDTH(i) ((i)>>4)
@@ -44,19 +54,6 @@ Dim TileY(byte index)
 #define MOD_TILE_WIDTH(i) ((i)&15)
 #define MOD_TILE_HEIGHT(i) ((i)&15)
 
-Dim TileX2(byte index)
-{
-	return MUL_TILE_WIDTH(GetCol(index));
-}
-Dim TileY2(byte index)
-{
-	return MUL_TILE_HEIGHT(GetRow(index));
-}
-
-
-//slide 12 dialeksh 8
-
-typedef unsigned short Index; // [MSB X][LSB Y]
 #define TILEX_MASK 0xFF00
 #define TILEX_SHIFT 8
 #define TILEY_MASK 0x00FF
@@ -69,9 +66,22 @@ Index MakeIndex2(byte row, byte col)
 {
 	return (MUL_TILE_WIDTH(col) << TILEX_SHIFT) | MUL_TILE_HEIGHT(row);
 }
-//changes target bitmap
 
-#define TILE_SCALE 4
+#define TILE_SCALE 2
+void BitmapBlit(ALLEGRO_BITMAP* sourceBitmap, Rect sourceRect, ALLEGRO_BITMAP* destinationBitmap, Point destPoint) {
+	al_set_target_bitmap(destinationBitmap);
+	al_draw_bitmap_region(
+		sourceBitmap,	//bitmap
+		sourceRect.x,	//sx
+		sourceRect.y,	//sy
+		sourceRect.w,	//sw
+		sourceRect.h,	//sh
+		destPoint.x,	//dx
+		destPoint.y,	//dy
+		0				//flags
+	);
+}
+
 void PutTile(ALLEGRO_BITMAP* dest, Dim x, Dim y, ALLEGRO_BITMAP* tiles, Index tile) {
 	al_set_target_bitmap(dest);
 	al_draw_bitmap_region(
@@ -87,36 +97,110 @@ void PutTile(ALLEGRO_BITMAP* dest, Dim x, Dim y, ALLEGRO_BITMAP* tiles, Index ti
 }
 
 
-//slide 16 dialeksh 8
-#define MAX_WIDTH 1024
-#define MAX_HEIGHT 256
 
-typedef Index TileMap[MAX_WIDTH][MAX_HEIGHT];
-static TileMap map; // example of a global static map
+Index GetTile(TileMap TileMapIndexes, Dim col, Dim row)
+{
+	return TileMapIndexes[row][col];
+}
+ALLEGRO_BITMAP* map = nullptr;
+TileMap TileMapIndexes;
+ALLEGRO_BITMAP* dpyBuffer = nullptr;
+ALLEGRO_BITMAP* tileset = nullptr;
+bool dpyChanged = true;
+Dim dpyX = 0, dpyY = 0;
+void TileTerrainDisplay(TileMap tileMapIndexes, ALLEGRO_BITMAP* dest, const Rect& viewWin, const Rect& displayArea) {
+	if (dpyChanged) {
+		auto startCol = DIV_TILE_WIDTH(viewWin.x);
+		auto startRow = DIV_TILE_HEIGHT(viewWin.y);
+		auto endCol = DIV_TILE_WIDTH(viewWin.x + viewWin.w - 1);
+		auto endRow = DIV_TILE_HEIGHT(viewWin.y + viewWin.h - 1);
+		dpyX = MOD_TILE_WIDTH(viewWin.x);
+		dpyY = MOD_TILE_WIDTH(viewWin.y);
+		dpyChanged = false;
+		dpyBuffer = al_create_bitmap(((endCol - startCol)+1) * 16, ((endRow - startRow) + 1)* 16);
+		for (Dim row = startRow; row <= endRow; ++row)
+			for (Dim col = startCol; col <= endCol; ++col)
+				PutTile(
+					dpyBuffer,
+					MUL_TILE_WIDTH(col - startCol),
+					MUL_TILE_HEIGHT(row - startRow),
+					tileset,
+					GetTile(tileMapIndexes, col, row)
+				);
+	}
+	BitmapBlit(
+		dpyBuffer,
+		{ dpyX, dpyY , viewWin.w, viewWin.h },
+		dest,
+		{ displayArea.x, displayArea.y }
+	);
+
+}
 
 void SetTile(TileMap* m, Dim col, Dim row, Index index)
 {
 	(*m)[row][col] = index;
 }
 
-Index GetTile(const TileMap* m, Dim col, Dim row)
-{
-	return (*m)[row][col];
+void WriteTextMap(const TileMap, FILE fp) {
+	/// custom write in text format /
 }
 
-//void WriteBinMap(const TileMap* m, FILE* fp) {
-//	fwrite((m, sizeof(TileMap), 1, fp);
-//}
+struct ViewWindow {
+	ALLEGRO_BITMAP* camera = nullptr;
+	Rect dimensions = Rect(0, 0, 160, 160);
+	Rect displayArea = Rect(0, 0, 0, 0);
 
-void ReadBinMap(TileMap* m, FILE* fp) {
-	/*binary formatted read, like descent parsing*/
+	ViewWindow() {}
+
+	ViewWindow(Rect dimensions, Rect displayArea) {
+		this->dimensions = dimensions;
+		this->displayArea = displayArea;
+		this->camera = al_create_bitmap(dimensions.h, dimensions.w);
+	}
+};
+
+int getColFromID(int ID, int TileSetWidth) {
+	return ID % TileSetWidth;
 }
 
-void WriteTextMap(const TileMap*, FILE* fp) {
-	/*custom write in text format*/
+int getRowFromID(int ID, int TileSetWidth) {
+	return ID / TileSetWidth;
 }
 
-bool ReadTextMap(TileMap* m, FILE* fp) {
-	/*parsing*/
-	return true;
+TileMap getTileMapIDs(std::string filename) {
+	std::string line;
+	std::vector<std::vector<int>> TileIDs;
+	TileMap tileIndexes;
+	std::ifstream input(filename);
+	int id;
+	if (input.is_open())
+		while (std::getline(input, line)) {
+			std::vector<int> row;
+			std::stringstream input_stringstream(line);
+			while (getline(input_stringstream, line, ',')) {
+				row.push_back(std::stoi(line));
+			}
+			TileIDs.push_back(row);
+		}
+	for (int i = 0; i < TileIDs.size(); i++) {
+		std::vector<Index> a;
+		tileIndexes.push_back(a);
+		for (int j = 0; j < TileIDs[i].size(); j++) {
+			tileIndexes[i].push_back(MakeIndex2(getRowFromID(TileIDs[i][j], 19), getColFromID(TileIDs[i][j], 19)));
+		}
+	}
+	return tileIndexes;
+}
+
+
+void getMapIndexes(TileMap& mapTileIndexes, TileMap tileMapIndexes) {
+	for (int i = 0; i < tileMapIndexes.size(); i++) {
+		std::vector<Index> a;
+		mapTileIndexes.push_back(a);
+		for (int j = 0; j < tileMapIndexes[i].size(); j++) {
+			PutTile(map, j * 16, i * 16, tileset, tileMapIndexes[i][j]);
+			mapTileIndexes[i].push_back(tileMapIndexes[i][j]);
+		}
+	}
 }
